31488f2c36d09725848d4ef3fb8108254a727d0d
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-22 14:56:40 -0400

    first migration

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Note(models.Model):
+  title = models.CharField(max_length=255, blank=False, null=False)
+  body = models.TextField(help_text="Type a note, if you dare", blank=False, null=False)
+  created_at = models.DateTimeField(default = timezone.now())
+  updated_at = models.DateTimeField(blank=True, null=True)
+
+  def __str__(self):
+    return self.title
+
+  def update(self):
+    self.updated_at = timezone.now()
+    self.save
+
